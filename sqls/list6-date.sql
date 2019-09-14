2f330858dcb9efacd5ab6891f15d0dcbd5e4506b
mytinylabo <mytinylabo@gmail.com>
2019-09-14 10:05:49 +0900

    Fix typo

--- sqls/list6-date.sql
+++ sqls/list6-date.sql
@@ -2,7 +2,7 @@ select current_date;
 select current_time;
 select current_timestamp;
 
-select current_timestample,
+select current_timestamp,
        extract(year   from current_timestamp) as year,
        extract(month  from current_timestamp) as month,
        extract(day    from current_timestamp) as day,
